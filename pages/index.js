a79f395f6cdb4c9f4416d0500988f448a9980a21
ebber <T4lkthr33>
2019-02-07 19:28:52 -0600

    just the title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Header from "../components/header";
 
+const CSS = "background: radial-gradient(circle closest-side, yellow, green);"
+
 export default () => (
   <main>
-    <Header />
-    <section>
-      <Link href="/about">
-        <a>Go to About Me</a>
-      </Link>
-    </section>
+    <style>{'body { background: #b0b1b2; width:100%; height:100%}'}</style>
+    <center><h1> Rackster Solutions </h1></center>
+    <center><h2> Coming Soon </h2></center>
   </main>
+
 );
